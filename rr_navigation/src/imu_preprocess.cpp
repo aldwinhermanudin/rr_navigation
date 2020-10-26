cc021af95bc58f374d4e39b27b8c3a302db8c728
Aldwin Hermanudin <aldwin@hermanudin.com>
2020-10-25 21:19:23 -0500

    update to use bosch bno055 and enable auto map creation

--- rr_navigation/src/imu_preprocess.cpp
+++ rr_navigation/src/imu_preprocess.cpp
@@ -1,62 +1,123 @@
 #include "ros/ros.h"
 #include "std_msgs/String.h"
 #include "sensor_msgs/Imu.h"
-#include "nav_msgs/Odometry.h"
-#include "sensor_msgs/LaserScan.h"
 #include <exception>
- 
+
+std::string src_topic;
+std::string dst_topic;
+std::vector<double> o_cov;
+std::vector<double> av_cov;
+std::vector<double> la_cov;
+
 ros::Subscriber sub_from;
 ros::Publisher pub_to;
 
-ros::Subscriber sub_scan_from;
-ros::Publisher pub_scan_to;
- 
-void ProcessOdomCallback( const nav_msgs::Odometry::ConstPtr& msg )
+
+std::vector<double> GetListParam( const ros::NodeHandle& n, 
+                                  const std::string& param_path )
 {
-    nav_msgs::Odometry msg_with_cov = *msg;
+    std::vector<double> ret;
+    if (n.getParam(param_path, ret))
+    {
+        ROS_INFO_STREAM("Got param from " << param_path);
+    } 
+    else 
+    {
+        ROS_ERROR_STREAM("Failed to get param " << param_path );
+        throw std::logic_error("failed to get param from " + param_path);
+    }
+    return ret;
+}
+
+std::string GetStringParam( const ros::NodeHandle& n, 
+                            const std::string& param_path )
+{
+    std::string ret;
+    if (n.getParam(param_path, ret))
+    {
+        ROS_INFO_STREAM("Got param from " << param_path);
+    } 
+    else 
+    {
+        ROS_ERROR_STREAM("Failed to get param " << param_path );
+        throw std::logic_error("failed to get param from " + param_path);
+    }
+    return ret;
+}
+
+void ProcessIMUCallback( const sensor_msgs::Imu::ConstPtr& msg )
+{
+    sensor_msgs::Imu msg_with_cov = *msg;
     
-    std::vector<double> pose_cov{0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0025, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0025};
-    std::vector<double> twist_cov{0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.25, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.02, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.02, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.02};
+    msg_with_cov.header.frame_id = "base_link";
 
-    msg_with_cov.header.frame_id = "odom";
-    msg_with_cov.child_frame_id = "base_footprint";
+    // not using orientation for now, but axis need to be switched.
 
- 
+    // switch x and y axis for linear accel
+    msg_with_cov.angular_velocity.y = msg->angular_velocity.x;
+    msg_with_cov.angular_velocity.x = (0.0 - msg->angular_velocity.y);
+
+    // switch x and y axis for linear accel
+    msg_with_cov.linear_acceleration.y = msg->linear_acceleration.x;
+    msg_with_cov.linear_acceleration.x = (0.0 - msg->linear_acceleration.y);
+
     // adding orientation covariance
-    memcpy( &msg_with_cov.pose.covariance[0],
-            &pose_cov[0], 
-            pose_cov.size() * sizeof(double));
+    memcpy( &msg_with_cov.orientation_covariance[0],
+            &o_cov[0], 
+            o_cov.size() * sizeof(double));
 
-    memcpy( &msg_with_cov.twist.covariance[0],
-            &twist_cov[0], 
-            twist_cov.size() * sizeof(double));
+    // adding angular_velocity covariance
+    memcpy( &msg_with_cov.angular_velocity_covariance[0],
+            &av_cov[0], 
+            av_cov.size() * sizeof(double));
+
+    // adding linear_acceleration covariance
+    memcpy( &msg_with_cov.linear_acceleration_covariance[0],
+            &la_cov[0], 
+            la_cov.size() * sizeof(double));
 
     // republishing imu data with covariance
     pub_to.publish(msg_with_cov);
 }
 
-void ProcessScanCallback( const sensor_msgs::LaserScan::ConstPtr& msg )
-{
-    sensor_msgs::LaserScan new_msg_frame = *msg;
-    
-    new_msg_frame.header.frame_id = "laser";
-
-    // republishing imu data with covariance
-    pub_scan_to.publish(new_msg_frame);
-}
-
 int main(int argc, char **argv)
 {
-    ros::init(argc, argv, "odom_scan_changer");
- 
+    ros::init(argc, argv, "smi_cov_adder");
+
     ros::NodeHandle n;
-    pub_to   = n.advertise<nav_msgs::Odometry>("/convert/odom", 1000);
-    sub_from = n.subscribe("/slamware_ros_sdk_server_node/odom", 1000, ProcessOdomCallback);   
-    
-    pub_scan_to   = n.advertise<sensor_msgs::LaserScan>("/scan", 1000);
-    sub_scan_from = n.subscribe("/slamware_ros_sdk_server_node/scan", 1000, ProcessScanCallback);
+    std::string ns = ros::this_node::getName();
+    std::string src_topic_path = "/from";
+    std::string dst_topic_path = "/to";
+    std::string o_cov_path = "/covariance/orientation";
+    std::string av_cov_path = "/covariance/angular_velocity";
+    std::string la_cov_path = "/covariance/linear_acceleration";
+
+    std::string src_topic_param =  ns + src_topic_path;
+    std::string dst_topic_param =  ns + dst_topic_path;
+    std::string o_cov_param =  ns + o_cov_path;
+    std::string av_cov_param = ns + av_cov_path;
+    std::string la_cov_param = ns + la_cov_path;
+
+    try
+    {
+        src_topic = GetStringParam( n, src_topic_param);
+        dst_topic = GetStringParam( n, dst_topic_param);
+        o_cov = GetListParam( n, o_cov_param);
+        av_cov = GetListParam( n, av_cov_param);
+        la_cov = GetListParam( n, la_cov_param);
+    }
+    catch (const std::exception& e)
+    {
+        ROS_ERROR_STREAM("Failed to get necessary param");
+        return -1;
+    }
+
+    ROS_INFO_STREAM("Subscribing to " << src_topic);
+    ROS_INFO_STREAM("Publishing to " << dst_topic);
+    pub_to   = n.advertise<sensor_msgs::Imu>(dst_topic, 1000);
+    sub_from = n.subscribe(src_topic, 1000, ProcessIMUCallback);
     
     ros::spin();
- 
+
     return 0;
 }
